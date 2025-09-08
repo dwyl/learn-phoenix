c910560e5d26c9355f3192a6d58587d4e44a2ffe
nelsonic <nelson+github@dwyl.com>
2025-09-08 11:44:15 +0100

    mix phx.gen.auth Accounts User users #160

--- examples/auth/lib/auth_web/router.ex
+++ examples/auth/lib/auth_web/router.ex
@@ -1,6 +1,8 @@
 defmodule AuthWeb.Router do
   use AuthWeb, :router
 
+  import AuthWeb.UserAuth
+
   pipeline :browser do
     plug :accepts, ["html"]
     plug :fetch_session
@@ -8,6 +10,7 @@ defmodule AuthWeb.Router do
     plug :put_root_layout, html: {AuthWeb.Layouts, :root}
     plug :protect_from_forgery
     plug :put_secure_browser_headers
+    plug :fetch_current_scope_for_user
   end
 
   pipeline :api do
@@ -40,5 +43,31 @@ defmodule AuthWeb.Router do
       live_dashboard "/dashboard", metrics: AuthWeb.Telemetry
       forward "/mailbox", Plug.Swoosh.MailboxPreview
     end
+  end
+
+  ## Authentication routes
+
+  scope "/", AuthWeb do
+    pipe_through [:browser, :redirect_if_user_is_authenticated]
+
+    get "/users/register", UserRegistrationController, :new
+    post "/users/register", UserRegistrationController, :create
+  end
+
+  scope "/", AuthWeb do
+    pipe_through [:browser, :require_authenticated_user]
+
+    get "/users/settings", UserSettingsController, :edit
+    put "/users/settings", UserSettingsController, :update
+    get "/users/settings/confirm-email/:token", UserSettingsController, :confirm_email
+  end
+
+  scope "/", AuthWeb do
+    pipe_through [:browser]
+
+    get "/users/log-in", UserSessionController, :new
+    get "/users/log-in/:token", UserSessionController, :confirm
+    post "/users/log-in", UserSessionController, :create
+    delete "/users/log-out", UserSessionController, :delete
   end
 end
