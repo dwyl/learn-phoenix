1685ec2934e20bea8afb6c4a6132e9b469cd2f63
nelsonic <nelson+github@dwyl.com>
2026-08-04 02:13:04 +0100

    remove outdated examples #166

--- hello/lib/hello_web/controllers/user_registration_html.ex
+++
@@ -1,5 +0,0 @@
-defmodule HelloWeb.UserRegistrationHTML do
-  use HelloWeb, :html
-
-  embed_templates "user_registration_html/*"
-end
--- hello/lib/hello_web/controllers/user_settings_html.ex
+++
@@ -1,5 +0,0 @@
-defmodule HelloWeb.UserSettingsHTML do
-  use HelloWeb, :html
-
-  embed_templates "user_settings_html/*"
-end
